f7edb5318b70c5f9887000d8fcb7ebe3a018bf30
Lucas Gordillo <lucas@Lucas-MacBook-Pro.local>
2022-05-27 22:09:11 -0300

    Fix android app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'evenTicket',
+  appId: 'org.eventicket.app',
+  appName: 'EvenTicket',
   webDir: 'www',
   bundledWebRuntime: false
 };
